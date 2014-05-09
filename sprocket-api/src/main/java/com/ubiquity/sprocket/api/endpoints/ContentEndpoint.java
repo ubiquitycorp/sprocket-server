96b7678c11f79cdaa4cd66bce15305ab8da1bdbb
Chris Econn <chris@niobiumsolutions.com>
2014-05-09 09:52:13 -0700

    Added video stub

--- sprocket-api/src/main/java/com/ubiquity/sprocket/api/endpoints/ContentEndpoint.java
+++ sprocket-api/src/main/java/com/ubiquity/sprocket/api/endpoints/ContentEndpoint.java
@@ -14,27 +14,49 @@ import com.ubiquity.social.domain.ContentProvider;
 import com.ubiquity.sprocket.api.dto.model.ImageDto;
 import com.ubiquity.sprocket.api.dto.model.VideoDto;
 
-@Path("/1.0/content")
+@Path("/1.0/content/providers")
 public class ContentEndpoint {
 
 	private JsonConverter jsonConverter = JsonConverter.getInstance();
-	
+
 	@GET
-	@Path("/videos")
+	@Path("/{contentProviderId}/videos")
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response ping() {
 		List<VideoDto> videos = new LinkedList<VideoDto>();
-		
-		videos.add(new VideoDto.Builder().contentProviderId(ContentProvider.YouTube.ordinal()).itemKey("").thumb(new ImageDto("")).build());
-		videos.add(new VideoDto.Builder().contentProviderId(ContentProvider.YouTube.ordinal()).itemKey("").thumb(new ImageDto("")).build());
-		videos.add(new VideoDto.Builder().contentProviderId(ContentProvider.YouTube.ordinal()).itemKey("").thumb(new ImageDto("")).build());
-		videos.add(new VideoDto.Builder().contentProviderId(ContentProvider.YouTube.ordinal()).itemKey("").thumb(new ImageDto("")).build());
-		videos.add(new VideoDto.Builder().contentProviderId(ContentProvider.YouTube.ordinal()).itemKey("").thumb(new ImageDto("")).build());
-		videos.add(new VideoDto.Builder().contentProviderId(ContentProvider.YouTube.ordinal()).itemKey("").thumb(new ImageDto("")).build());
+
+		videos.add(new VideoDto.Builder()
+		.contentProviderId(ContentProvider.YouTube.ordinal())
+		.itemKey("UC_x5XG1OV2P6uZZ5FSM9Ttw")
+		.thumb(new ImageDto("https://yt3.ggpht.com/-Fgp8KFpgQqE/AAAAAAAAAAI/AAAAAAAAAAA/Wyh1vV5Up0I/s88-c-k-no/photo.jpg"))
+		.title("Google Developers")
+		.description("Talks, screencasts, interviews, and more relevant to Google's developer products.")
+		.build());
+		videos.add(new VideoDto.Builder()
+		.contentProviderId(ContentProvider.YouTube.ordinal())
+		.itemKey("UC_x5XG1OV2P6uZZ5FSM9Ttw")
+		.thumb(new ImageDto("https://yt3.ggpht.com/-Fgp8KFpgQqE/AAAAAAAAAAI/AAAAAAAAAAA/Wyh1vV5Up0I/s88-c-k-no/photo.jpg"))
+		.title("Google Developers")
+		.description("Talks, screencasts, interviews, and more relevant to Google's developer products.")
+		.build());
+		videos.add(new VideoDto.Builder()
+		.contentProviderId(ContentProvider.YouTube.ordinal())
+		.itemKey("UC_x5XG1OV2P6uZZ5FSM9Ttw")
+		.thumb(new ImageDto("https://yt3.ggpht.com/-Fgp8KFpgQqE/AAAAAAAAAAI/AAAAAAAAAAA/Wyh1vV5Up0I/s88-c-k-no/photo.jpg"))
+		.title("Google Developers")
+		.description("Talks, screencasts, interviews, and more relevant to Google's developer products.")
+		.build());
+		videos.add(new VideoDto.Builder()
+		.contentProviderId(ContentProvider.YouTube.ordinal())
+		.itemKey("UC_x5XG1OV2P6uZZ5FSM9Ttw")
+		.thumb(new ImageDto("https://yt3.ggpht.com/-Fgp8KFpgQqE/AAAAAAAAAAI/AAAAAAAAAAA/Wyh1vV5Up0I/s88-c-k-no/photo.jpg"))
+		.title("Google Developers")
+		.description("Talks, screencasts, interviews, and more relevant to Google's developer products.")
+		.build());
 
 		return Response.ok().entity(jsonConverter.convertToPayload(videos)).build();
 	}
-	
+
 }
 
 
