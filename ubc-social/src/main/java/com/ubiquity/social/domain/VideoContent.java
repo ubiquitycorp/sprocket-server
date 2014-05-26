0ecbeef26e8e71e500045611996867d561a6df00
Chris Econn <chris@niobiumsolutions.com>
2014-05-26 16:55:36 -0700

    Not sure what mods were made :/

--- ubc-social/src/main/java/com/ubiquity/social/domain/VideoContent.java
+++ ubc-social/src/main/java/com/ubiquity/social/domain/VideoContent.java
@@ -3,6 +3,8 @@ package com.ubiquity.social.domain;
 import javax.persistence.Column;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
@@ -13,6 +15,11 @@ import com.ubiquity.media.domain.Video;
 @Entity
 @Table(name = "video_content")
 public class VideoContent {
+
+	@Id
+	@GeneratedValue
+	@Column(name = "video_content_id")
+	private Long videoContentId;
 
 	@ManyToOne
 	@JoinColumn(name = "identity_id")
@@ -41,6 +48,10 @@ public class VideoContent {
 		this.identity = identity;
 		this.video = video;
 		this.lastUpdated = lastUpdated;
+	}
+
+	public Long getVideoContentId() {
+		return videoContentId;
 	}
 
 	public ExternalIdentity getIdentity() {
@@ -72,6 +83,7 @@ public class VideoContent {
 	}
 
 	public static class Builder {
+		private Long videoContentId;
 		private ExternalIdentity identity;
 		private Video video;
 		private Image thumb;
@@ -79,6 +91,11 @@ public class VideoContent {
 		private String title;
 		private String description;
 		private String category;
+
+		public Builder videoContentId(Long videoContentId) {
+			this.videoContentId = videoContentId;
+			return this;
+		}
 
 		public Builder identity(ExternalIdentity identity) {
 			this.identity = identity;
@@ -121,6 +138,7 @@ public class VideoContent {
 	}
 
 	private VideoContent(Builder builder) {
+		this.videoContentId = builder.videoContentId;
 		this.identity = builder.identity;
 		this.video = builder.video;
 		this.thumb = builder.thumb;
