816fbdc63a13ec2c7b7476b85ff8058426876a1a
Chris Econn <chris@niobiumsolutions.com>
2014-05-27 09:47:51 -0700

    Added search keys and increased patch

---
+++ sprocket-core/src/main/java/com/ubiquity/sprocket/search/SearchKeys.java
@@ -0,0 +1,28 @@
+package com.ubiquity.sprocket.search;
+
+public class SearchKeys {
+
+	public static class CommonFields {
+		public static final String FIELD_TITLE = "title";
+		public static final String FIELD_DESCRIPTION = "description";
+		public static final String FIELD_USER_ID = "user_id";
+		public static final String FIELD_ID = "id";
+		public static final String FIELD_DATA_TYPE = "data_type";
+		public static final String FIELD_BODY = "body";
+		public static final String FIELD_THUMBNAIL = "thumbnail";
+	}
+	
+	public static class VideoContentFields {
+		public static final String FIELD_CATEGORY = "category";
+		public static final String FIELD_ITEM_KEY = "item_key";
+	}
+	
+	public static class MessageContentFields {
+		public static final String FIELD_SENDER = "sender";
+	}
+	
+	public static class ActivityContentFields {
+		public static final String FIELD_POSTED_BY = "posted_by";
+	}
+	
+}
