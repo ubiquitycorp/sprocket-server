c4f8e140bad93b3cbde6b0605bcbd7c26df0fcfb
Chris Econn <chris@niobiumsolutions.com>
2014-06-01 11:18:48 -0700

    Added text/plain consumes annotation

--- sprocket-core/src/main/java/com/ubiquity/content/api/youtube/endpoints/YouTubeApiEndpoints.java
+++ sprocket-core/src/main/java/com/ubiquity/content/api/youtube/endpoints/YouTubeApiEndpoints.java
@@ -1,5 +1,6 @@
 package com.ubiquity.content.api.youtube.endpoints;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
 import javax.ws.rs.Path;
@@ -14,6 +15,7 @@ public interface YouTubeApiEndpoints {
     @GET
     @Path("v3/videos")
     @Produces(MediaType.TEXT_PLAIN)
+    @Consumes(MediaType.TEXT_PLAIN)
     ClientResponse<String> getVideos(@QueryParam("part") String part, @QueryParam("chart") String chart, @QueryParam("key") String apiKey, @HeaderParam("Authorization") String accessToken);
     
  
