eb413530c135d500a2cc33c437393674c2c4f5b1
Chris Econn <chris@niobiumsolutions.com>
2014-06-01 09:18:49 -0700

    Added jaxon provider

--- sprocket-core/src/main/java/com/ubiquity/content/api/youtube/endpoints/YouTubeApiEndpoints.java
+++ sprocket-core/src/main/java/com/ubiquity/content/api/youtube/endpoints/YouTubeApiEndpoints.java
@@ -3,6 +3,7 @@ package com.ubiquity.content.api.youtube.endpoints;
 import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 
 import org.jboss.resteasy.client.ClientResponse;
@@ -11,6 +12,7 @@ public interface YouTubeApiEndpoints {
 
     @GET
     @Path("v3/videos")
+    @Produces("application/json")
     ClientResponse<String> getVideos(@QueryParam("part") String part, @QueryParam("chart") String chart, @QueryParam("key") String apiKey, @HeaderParam("Authorization") String accessToken);
     
  
