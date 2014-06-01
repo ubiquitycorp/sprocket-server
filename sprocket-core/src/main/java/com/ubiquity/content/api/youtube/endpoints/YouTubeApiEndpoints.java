c069a640213044049fe58add2979a66e2d326374
Chris Econn <chris@niobiumsolutions.com>
2014-06-01 09:47:09 -0700

    Added text plain to get rid of message body errors

--- sprocket-core/src/main/java/com/ubiquity/content/api/youtube/endpoints/YouTubeApiEndpoints.java
+++ sprocket-core/src/main/java/com/ubiquity/content/api/youtube/endpoints/YouTubeApiEndpoints.java
@@ -5,6 +5,7 @@ import javax.ws.rs.HeaderParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
 
 import org.jboss.resteasy.client.ClientResponse;
 
@@ -12,7 +13,7 @@ public interface YouTubeApiEndpoints {
 
     @GET
     @Path("v3/videos")
-    @Produces("application/json")
+    @Produces(MediaType.TEXT_PLAIN)
     ClientResponse<String> getVideos(@QueryParam("part") String part, @QueryParam("chart") String chart, @QueryParam("key") String apiKey, @HeaderParam("Authorization") String accessToken);
     
  
