2b3901e2b52834e7306e431640d3db2d8730ef63
Chris Econn <chris@niobiumsolutions.com>
2014-06-05 12:49:43 -0700

    Increased patch to force deploy new hibernate session close issue

--- sprocket-core/src/main/java/com/ubiquity/content/api/youtube/endpoints/YouTubeApiEndpoints.java
+++ sprocket-core/src/main/java/com/ubiquity/content/api/youtube/endpoints/YouTubeApiEndpoints.java
@@ -13,7 +13,7 @@ public interface YouTubeApiEndpoints {
     @GET
     @Path("v3/videos")
     @Produces(MediaType.APPLICATION_JSON)
-    Response getVideos(@QueryParam("part") String part, @QueryParam("chart") String chart, @QueryParam("key") String apiKey, @HeaderParam("Authorization") String accessToken);
+    Response getVideos(@QueryParam("part") String part, @QueryParam("chart") String chart, @QueryParam("maxResults") Integer maxResults, @QueryParam("key") String apiKey, @HeaderParam("Authorization") String accessToken);
     
  
 }
