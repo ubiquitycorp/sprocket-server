848807909e5a6f5ae1555f59a00ab7c6cce1a21c
Mina Gerges <mina.gerges@globalimpactsw.com>
2014-06-08 18:25:31 +0300

    Added last-modified header to messages end point Changed version to "1.1.1"

--- sprocket-api/src/main/java/com/ubiquity/sprocket/api/endpoints/SocialEndpoint.java
+++ sprocket-api/src/main/java/com/ubiquity/sprocket/api/endpoints/SocialEndpoint.java
@@ -39,8 +39,6 @@ public class SocialEndpoint {
 
 		SocialNetwork socialNetwork = SocialNetwork.getEnum(socialProviderId);
 
-//		ExternalIdentity identity = ServiceFactory.getSocialService().findSocialIdentity(userId, socialNetwork);
-//		ServiceFactory.getSocialService().syncActivities(identity, socialNetwork);
 		CollectionVariant<Activity> variant = ServiceFactory.getSocialService().findActivityByOwnerIdAndSocialNetwork(userId, socialNetwork, ifModifiedSince);
 
 		// Throw a 304 if if there is no variant (no change)
@@ -87,6 +85,7 @@ public class SocialEndpoint {
 			result.getMessages().add(DtoAssembler.assemble(message));			
 
 		return Response.ok()
+				.header("Last-Modified", variant.getLastModified())
 				.entity(jsonConverter.convertToPayload(result))
 				.build();
 	}
