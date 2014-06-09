b2dfa5dc52cb610aaea5afb36738c98255c0404e
Chris Econn <chris@niobiumsolutions.com>
2014-06-08 21:34:09 -0700

    Added limit to activities and messages

--- sprocket-api/src/main/java/com/ubiquity/sprocket/api/endpoints/SocialEndpoint.java
+++ sprocket-api/src/main/java/com/ubiquity/sprocket/api/endpoints/SocialEndpoint.java
@@ -13,7 +13,6 @@ import org.slf4j.LoggerFactory;
 
 import com.niobium.common.serialize.JsonConverter;
 import com.niobium.repository.CollectionVariant;
-import com.ubiquity.identity.domain.ExternalIdentity;
 import com.ubiquity.social.domain.Activity;
 import com.ubiquity.social.domain.Message;
 import com.ubiquity.social.domain.SocialNetwork;
@@ -45,8 +44,13 @@ public class SocialEndpoint {
 		if (variant == null)
 			return Response.notModified().build();
 
-		for(Activity activity : variant.getCollection())
-			results.getActivities().add(DtoAssembler.assemble(activity));
+		int limit = 20;
+		int count = 0;
+		for(Activity activity : variant.getCollection()) {
+			if(count < limit)
+				results.getActivities().add(DtoAssembler.assemble(activity));
+			count++;
+		}
 
 		return Response.ok()
 				.header("Last-Modified", variant.getLastModified())
@@ -69,21 +73,24 @@ public class SocialEndpoint {
 		MessagesDto result = new MessagesDto();
 
 		SocialNetwork socialNetwork = SocialNetwork.getEnum(socialProviderId);
-		//test temporarily
-		/*ExternalIdentity identity = SocialService.getAssociatedSocialIdentity(user, socialNetwork);
-		ServiceFactory.getSocialService().sync(identity, socialNetwork);*/
-				 
-		
+					 
 		CollectionVariant<Message> variant = ServiceFactory.getSocialService().findMessagesByOwnerIdAndSocialNetwork(userId, socialNetwork, ifModifiedSince);
 		
 		
 		// Throw a 304 if if there is no variant (no change)
 		if (variant == null)
 			return Response.notModified().build();
-		// prune this before sending to search index
-		for(Message message : variant.getCollection())
-			result.getMessages().add(DtoAssembler.assemble(message));			
+		
+		// limit 20 for demo
+		int limit = 20;
+		int count = 0;
+		for(Message message : variant.getCollection()) {
+			if(count < limit)
+				result.getMessages().add(DtoAssembler.assemble(message));
+			count++;
+		}
 
+		
 		return Response.ok()
 				.header("Last-Modified", variant.getLastModified())
 				.entity(jsonConverter.convertToPayload(result))
