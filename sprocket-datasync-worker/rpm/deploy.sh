96376c6b68dd325e6e485d7a04e8e56247b97db7
Chris Econn <chris@niobiumsolutions.com>
2014-06-21 12:08:06 -0700

    Added restart command if service is running

--- sprocket-datasync-worker/rpm/deploy.sh
+++ sprocket-datasync-worker/rpm/deploy.sh
@@ -2,4 +2,6 @@ service=sprocket-datasync-worker
 if (( $(ps -ef | grep -v grep | grep $service | wc -l) == 0 ))
 then
 /etc/init.d/$service start
+else
+/etc/init.d/$service restart
 fi
--- sprocket-tracker-worker/rpm/deploy.sh
+++ sprocket-tracker-worker/rpm/deploy.sh
@@ -2,4 +2,6 @@ service=sprocket-tracker-worker
 if (( $(ps -ef | grep -v grep | grep $service | wc -l) == 0 ))
 then
 /etc/init.d/$service start
+else
+/etc/init.d/$service restart
 fi
