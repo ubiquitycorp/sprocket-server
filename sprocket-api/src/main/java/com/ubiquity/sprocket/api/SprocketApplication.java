5d48aca5717c171414f3f96d750ca51a9e9c77e9
engpeter2010 <peter_aboalim@yahoo.com>
2014-07-01 17:17:15 +0200

    Upgraded resteasy version to 3.0.8.Final Modified SprocketApplication.java and web.xml

--- sprocket-api/src/main/java/com/ubiquity/sprocket/api/SprocketApplication.java
+++ sprocket-api/src/main/java/com/ubiquity/sprocket/api/SprocketApplication.java
@@ -1,9 +1,34 @@
 package com.ubiquity.sprocket.api;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
+import com.ubiquity.sprocket.api.endpoints.AnalyticsEndpoint;
+import com.ubiquity.sprocket.api.endpoints.ContentEndpoint;
+import com.ubiquity.sprocket.api.endpoints.DocumentsEndpoint;
+import com.ubiquity.sprocket.api.endpoints.SocialEndpoint;
+import com.ubiquity.sprocket.api.endpoints.UsersEndpoint;
+
 @ApplicationPath("services")
 public class SprocketApplication extends Application {
+	
+	 private Set<Object> singletons = new HashSet<Object>();
+
+	  public SprocketApplication() {
+		  
+	    singletons.add(new DocumentsEndpoint());
+	    singletons.add(new AnalyticsEndpoint());
+	    singletons.add(new SocialEndpoint());
+	    singletons.add(new UsersEndpoint());
+	    singletons.add(new ContentEndpoint());
+	  }
+
+	  @Override
+	  public Set<Object> getSingletons() {
+	    return singletons;
+	  }
 
 }
