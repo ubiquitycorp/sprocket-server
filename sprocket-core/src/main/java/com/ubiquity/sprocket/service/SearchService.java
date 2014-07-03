368582f2a1edef3ee7ee991e7040eb54ba3c964d
engpeter2010 <peter_aboalim@yahoo.com>
2014-07-03 13:44:10 +0200

    Changed sprocket-api version to "1.1.10" Changed sprocket-datasync version to "0.1.45" Only added Category to search keys if it is not null

--- sprocket-core/src/main/java/com/ubiquity/sprocket/service/SearchService.java
+++ sprocket-core/src/main/java/com/ubiquity/sprocket/service/SearchService.java
@@ -104,7 +104,8 @@ public class SearchService {
 			if(videoContent.getThumb() != null)
 				document.getFields().put(SearchKeys.CommonFields.FIELD_THUMBNAIL, videoContent.getThumb().getUrl());
 
-			document.getFields().put(SearchKeys.VideoContentFields.FIELD_CATEGORY, videoContent.getCategory());
+			if(videoContent.getCategory() != null)
+				document.getFields().put(SearchKeys.VideoContentFields.FIELD_CATEGORY, videoContent.getCategory());
 			document.getFields().put(SearchKeys.VideoContentFields.FIELD_ITEM_KEY, videoContent.getVideo().getItemKey());
 			
 			document.getFields().put(SearchKeys.CommonFields.FIELD_DATA_TYPE, VideoContent.class.getSimpleName());
