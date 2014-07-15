40e0010b4b47fc0d673f3267aa5b242c61698364
minashafikthabet <mina.shafik.thabet@gmail.com>
2014-07-15 16:22:39 +0200

    added list activities test

--- sprocket-core/src/test/java/com/ubiquity/social/api/TwitterApiTest.java
+++ sprocket-core/src/test/java/com/ubiquity/social/api/TwitterApiTest.java
@@ -4,6 +4,7 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
 import org.junit.Assert;
 import org.junit.BeforeClass;
@@ -20,7 +21,6 @@ import com.ubiquity.social.domain.Activity;
 import com.ubiquity.social.domain.ActivityType;
 import com.ubiquity.social.domain.Message;
 import com.ubiquity.social.domain.PostActivity;
-import com.ubiquity.sprocket.service.ServiceFactory;
 
 public class TwitterApiTest {
 private static Logger log = LoggerFactory.getLogger(VimeoAPITest.class);
@@ -31,11 +31,20 @@ private static Logger log = LoggerFactory.getLogger(VimeoAPITest.class);
 	public static void setUp() throws Exception {
 //		identity = ServiceFactory.getExternalIdentityService().findExternalIdentity((long)1, ExternalNetwork.Twitter);
 //		log.debug("authenticated Twitter with identity {} ", identity);
+		User user = new User.Builder()
+		.lastUpdated(System.currentTimeMillis())
+		.firstName(UUID.randomUUID().toString())
+		.lastName(UUID.randomUUID().toString())
+		.email(UUID.randomUUID().toString())
+		.clientPlatform(ClientPlatform.Android)
+		.displayName(UUID.randomUUID().toString())
+		.build();
 		
 		identity = new ExternalIdentity.Builder()
 				.accessToken("2576165924-bjuqdtF54hoIw4fufobnX6O6DCaHfFhp4riitH1")
 				.secretToken("8StcfxfvMzdyuUFRcmf7dtn9kI1VTAvFCoB0deZZy8qkW")
 				.identifier("2576165924")
+				.user(user)
 				.build();
 		log.debug("authenticated Twitter with identity {} ", identity);
 	}
@@ -79,7 +88,7 @@ private static Logger log = LoggerFactory.getLogger(VimeoAPITest.class);
 		Boolean sent = twitterAPI.postActivity(identity, postActivity);
 		Assert.assertTrue(sent);
 	}
-	//@Test
+	@Test
 	public void listActivities() {
 		
 		SocialAPI twitterAPI = SocialAPIFactory.createProvider(ExternalNetwork.Twitter, ClientPlatform.WEB);
