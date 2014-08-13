75daae6e26725d70e766bf0222ca6c2e0caccb68
engpeter2010 <peter_aboalim@yahoo.com>
2014-08-13 16:14:27 +0200

    Added check for indexed image of message

--- sprocket-core/src/test/java/com/ubiquity/sprocket/IntegrationTestSuite.java
+++ sprocket-core/src/test/java/com/ubiquity/sprocket/IntegrationTestSuite.java
@@ -4,14 +4,12 @@ import org.apache.commons.configuration.Configuration;
 import org.apache.commons.configuration.PropertiesConfiguration;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 import com.niobium.repository.jpa.EntityManagerSupport;
 import com.niobium.repository.redis.JedisConnectionFactory;
-import com.ubiquity.social.api.TwitterApiTest;
 import com.ubiquity.social.repository.ActivityRepositoryTest;
 import com.ubiquity.social.repository.MessageRepositoryTest;
 import com.ubiquity.sprocket.repository.SocialRepositoryTest;
