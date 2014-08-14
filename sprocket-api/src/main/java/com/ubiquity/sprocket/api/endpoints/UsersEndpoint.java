c6abbd039754c09d7b42fe614aeb965ef616dc3f
minashafikthabet <mina.shafik.thabet@gmail.com>
2014-08-14 11:53:56 +0200

    refix twitter login

--- sprocket-api/src/main/java/com/ubiquity/sprocket/api/endpoints/UsersEndpoint.java
+++ sprocket-api/src/main/java/com/ubiquity/sprocket/api/endpoints/UsersEndpoint.java
@@ -349,16 +349,13 @@ public class UsersEndpoint {
 		} else if (externalNetwork.network == Network.Social) {
 			SocialAPI socialApi = SocialAPIFactory.createProvider(externalNetwork, clientPlatform);
 			String redirectUri = null;
-			String verifier = identityDto.getOauthVerifier();
-			if(externalNetwork.equals(ExternalNetwork.Google) || externalNetwork.equals(ExternalNetwork.YouTube))
+			if((externalNetwork.equals(ExternalNetwork.Google) || externalNetwork.equals(ExternalNetwork.YouTube)) && clientPlatform.equals(ClientPlatform.WEB))
 			{
-				if(clientPlatform.equals(ClientPlatform.WEB))
-					redirectUri = "postmessage";
-				verifier=identityDto.getCode();
+				redirectUri = "postmessage";
 			}
 			// the expiredAt value in externalIdentity object returned from getAccessToken() is equal to expiresIn value
 			ExternalIdentity externalidentity = socialApi.getAccessToken(
-					verifier,
+					identityDto.getCode(),
 					identityDto.getOauthToken(),
 					identityDto.getOauthTokenSecret(), redirectUri);
 
