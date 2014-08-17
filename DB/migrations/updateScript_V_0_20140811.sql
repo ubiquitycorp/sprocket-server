9c05f5aa06784a5c5df637cb708add4a6ee7b670
minashafikthabet <mina.shafik.thabet@gmail.com>
2014-08-17 16:55:57 +0200

    added incises in external-identity table

--- DB/migrations/updateScript_V_0_20140811.sql
+++ DB/migrations/updateScript_V_0_20140811.sql
@@ -4,4 +4,6 @@ ALTER IGNORE TABLE user ADD COLUMN last_login bigint(20) NOT NULL;
 UPDATE user SET last_login = UNIX_TIMESTAMP()*1000 WHERE last_login =0;
 --- change updated naming error from 'last_udpated' to 'last_updated'
 ALTER TABLE user CHANGE last_udpated last_updated bigint(20) NOT NULL;
-ALTER TABLE identity CHANGE last_udpated last_updated bigint(20) NOT NULL
+ALTER TABLE identity CHANGE last_udpated last_updated bigint(20) NOT NULL;
+ALTER TABLE external_identity ADD INDEX identifier (idx_external_identifier);
+ALTER TABLE external_identity ADD INDEX external_network (idx_external_network);
