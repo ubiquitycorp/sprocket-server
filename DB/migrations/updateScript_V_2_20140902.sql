5a128c7507ad7f1a314396a9906ae486c515bdf8
minashafikthabet <mina.shafik.thabet@gmail.com>
2014-09-03 14:43:51 +0300

    added updated statement to set category id in videos

--- DB/migrations/updateScript_V_2_20140902.sql
+++ DB/migrations/updateScript_V_2_20140902.sql
@@ -1,3 +1,4 @@
 USE sprocket
 ALTER TABLE video_content change category category_external_identifier varchar(255);
 ALTER TABLE video_content ADD COLUMN category int(11); 
+update Table video_content set category =0
