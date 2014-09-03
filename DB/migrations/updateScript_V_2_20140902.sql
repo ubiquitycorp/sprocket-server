d63cfcbd4202d2835fcb4fd1c0067eb1d8461e95
engpeter2010 <peter_aboalim@yahoo.com>
2014-09-03 11:40:58 +0300

    Added location columns to updateScript_V_2_20140902.sql

--- DB/migrations/updateScript_V_2_20140902.sql
+++ DB/migrations/updateScript_V_2_20140902.sql
@@ -1,3 +1,6 @@
 USE sprocket
 ALTER TABLE video_content change category category_external_identifier varchar(255);
 ALTER TABLE video_content ADD COLUMN category int(11); 
+ALTER TABLE user ADD COLUMN latitude double; 
+ALTER TABLE user ADD COLUMN longitude double; 
+ALTER TABLE user ADD COLUMN location_timestamp bigint(20); 
