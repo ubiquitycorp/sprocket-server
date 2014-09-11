b329aaa70e67240e46bebcbd925767251f46c380
minashafikthabet <mina.shafik.thabet@gmail.com>
2014-09-11 15:39:26 +0300

    reset recomendation endpoint class

--- sprocket-api/src/main/java/com/ubiquity/sprocket/api/endpoints/AnalyticsEndpoint.java
+++ sprocket-api/src/main/java/com/ubiquity/sprocket/api/endpoints/AnalyticsEndpoint.java
@@ -1,5 +1,7 @@
 package com.ubiquity.sprocket.api.endpoints;
 
+import java.util.Collection;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
 import javax.ws.rs.Path;
@@ -9,8 +11,15 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import com.niobium.common.serialize.JsonConverter;
+import com.niobium.repository.CollectionVariant;
+import com.ubiquity.content.domain.VideoContent;
+import com.ubiquity.external.domain.ExternalNetwork;
+import com.ubiquity.social.domain.Activity;
+import com.ubiquity.sprocket.api.DtoAssembler;
 import com.ubiquity.sprocket.api.dto.containers.RecommendationsDto;
 import com.ubiquity.sprocket.api.interceptors.Secure;
+import com.ubiquity.sprocket.service.AnalyticsService;
+import com.ubiquity.sprocket.service.ServiceFactory;
 
 @Path("/1.0/analytics")
 public class AnalyticsEndpoint {
@@ -22,9 +31,7 @@ public class AnalyticsEndpoint {
 	@Produces(MediaType.APPLICATION_JSON)
 	@Secure
 	public Response recommendations(@PathParam("userId") Long userId) {
-		//throw new UnsupportedOperationException("This endpoint is currently not supported");
-		RecommendationsDto recommendationsDto = new RecommendationsDto();
-		return Response.ok().entity(jsonConverter.convertToPayload(recommendationsDto)).build();
+		throw new UnsupportedOperationException("This endpoint is currently not supported");
 	}
 	
 	@GET
@@ -34,7 +41,7 @@ public class AnalyticsEndpoint {
 	public Response recommendedActivitiesByProvider(@PathParam("userId") Long userId, @PathParam("externalNetworkId") Integer externalNetworkId, @HeaderParam("If-Modified-Since") Long ifModifiedSince) {
 
 		RecommendationsDto recommendationsDto = new RecommendationsDto();
-		/*AnalyticsService analyticsService = ServiceFactory.getAnalyticsService();
+		AnalyticsService analyticsService = ServiceFactory.getAnalyticsService();
 		
 		ExternalNetwork externalNetwork = ExternalNetwork.getNetworkById(externalNetworkId);
 		
@@ -50,9 +57,9 @@ public class AnalyticsEndpoint {
 		for(Activity activity : activities) {
 			recommendationsDto.getActivities().add(DtoAssembler.assemble(activity));
 		}
-	*/
+	
 		return Response.ok()
-			//	.header("Last-Modified", variant.getLastModified())
+				.header("Last-Modified", variant.getLastModified())
 				.entity(jsonConverter.convertToPayload(recommendationsDto))
 				.build();
 	}
@@ -64,7 +71,7 @@ public class AnalyticsEndpoint {
 	public Response recommendedVideosByProvider(@PathParam("userId") Long userId, @PathParam("externalNetworkId") Integer externalNetworkId, @HeaderParam("If-Modified-Since") Long ifModifiedSince) {
 
 		RecommendationsDto recommendationsDto = new RecommendationsDto();
-		/*AnalyticsService analyticsService = ServiceFactory.getAnalyticsService();
+		AnalyticsService analyticsService = ServiceFactory.getAnalyticsService();
 		
 		ExternalNetwork externalNetwork = ExternalNetwork.getNetworkById(externalNetworkId);
 		
@@ -80,39 +87,9 @@ public class AnalyticsEndpoint {
 		for(VideoContent videoContent : videos) {
 			recommendationsDto.getVideos().add(DtoAssembler.assemble(videoContent));
 		}
-	*/
-		return Response.ok()
-				//.header("Last-Modified", variant.getLastModified())
-				.entity(jsonConverter.convertToPayload(recommendationsDto))
-				.build();
-	}
 	
-	@GET
-	@Path("users/{userId}/providers/{externalNetworkId}/recommendations")
-	@Produces(MediaType.APPLICATION_JSON)
-	@Secure
-	public Response recommendedByProvider(@PathParam("userId") Long userId, @PathParam("externalNetworkId") Integer externalNetworkId, @HeaderParam("If-Modified-Since") Long ifModifiedSince) {
-
-		RecommendationsDto recommendationsDto = new RecommendationsDto();
-		/*AnalyticsService analyticsService = ServiceFactory.getAnalyticsService();
-		
-		ExternalNetwork externalNetwork = ExternalNetwork.getNetworkById(externalNetworkId);
-		
-		CollectionVariant<VideoContent> variant = analyticsService.findAllRecommendedVideos(userId, externalNetwork, ifModifiedSince);
-		
-		
-		// Throw a 304 if if there is no variant (no change)
-		if (variant == null)
-			return Response.notModified().build();
-		
-		
-		Collection<VideoContent> videos = variant.getCollection();
-		for(VideoContent videoContent : videos) {
-			recommendationsDto.getVideos().add(DtoAssembler.assemble(videoContent));
-		}
-	*/
 		return Response.ok()
-				//.header("Last-Modified", variant.getLastModified())
+				.header("Last-Modified", variant.getLastModified())
 				.entity(jsonConverter.convertToPayload(recommendationsDto))
 				.build();
 	}
