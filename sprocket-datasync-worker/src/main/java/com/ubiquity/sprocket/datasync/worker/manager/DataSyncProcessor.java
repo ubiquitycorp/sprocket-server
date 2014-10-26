0f79256154283505baa9224c337079542b286ac7
minashafikthabet <mina.shafik.thabet@gmail.com>
2014-10-26 19:54:44 +0200

    uncommented linkedin sync

--- sprocket-datasync-worker/src/main/java/com/ubiquity/sprocket/datasync/worker/manager/DataSyncProcessor.java
+++ sprocket-datasync-worker/src/main/java/com/ubiquity/sprocket/datasync/worker/manager/DataSyncProcessor.java
@@ -142,13 +142,13 @@ public class DataSyncProcessor extends Thread {
 			log.info("Processed {} messages in {} seconds", n, new Period(start, new DateTime()).getSeconds());
 			sendStepCompletedMessageToIndividual(backchannel, externalNetwork, "Synchronized messages", getResoursePath(userId, externalNetwork, ResourceType.messages), n, userId);
 
+//		}|| externalNetwork.equals(ExternalNetwork.Tumblr)
+		} else if(externalNetwork.equals(ExternalNetwork.LinkedIn)) {
+			DateTime start = new DateTime();
+			int n = processActivities(identity, externalNetwork);
+			log.info("Processed {} local activities in {} seconds", n, new Period(start, new DateTime()).getSeconds());
+			sendStepCompletedMessageToIndividual(backchannel, externalNetwork, "Synchronized feed", getResoursePath(userId, externalNetwork, ResourceType.activities), n, userId);
 		}
-//		} else if(externalNetwork.equals(ExternalNetwork.LinkedIn) || externalNetwork.equals(ExternalNetwork.Tumblr)) {
-//			DateTime start = new DateTime();
-//			int n = processActivities(identity, externalNetwork);
-//			log.info("Processed {} local activities in {} seconds", n, new Period(start, new DateTime()).getSeconds());
-//			sendStepCompletedMessageToIndividual(backchannel, externalNetwork, "Synchronized feed", getResoursePath(userId, externalNetwork, ResourceType.activities), n, userId);
-//		}
 		
 		sendSyncCompletedMessageToIndividual(backchannel, externalNetwork, userId);
 	}
