abc1d20aab5a53e938f5532d3a96337d11d83741
minashafikthabet <mina.shafik.thabet@gmail.com>
2014-11-09 11:02:53 +0200

    added Comments table and unit test to test the comments addition

--- DB/delete all tables.sql
+++ DB/delete all tables.sql
@@ -12,6 +12,8 @@ delete from recommended_video;
 delete from engaged_item;
 delete from group_membership;
 #activities,messages and vedios 
+update comment set parent_id = null;
+delete from comment ;
 delete from activity ;
 delete from message;
 delete from conversation_contact;
@@ -24,7 +26,7 @@ delete from user_location;
 #delete from place where parent_id is not null;
 #identities and users
 
-delete from external_identity;
-delete from native_identity;
-delete from identity;
-delete from user;
+#delete from external_identity;
+#delete from native_identity;
+#delete from identity;
+#delete from user;
