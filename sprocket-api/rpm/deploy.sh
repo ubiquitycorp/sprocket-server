99386410e58931485704e38fe80a7900c8419b5d
Jonathan Kalbfeld <jonathan.kalbfeld@thoughtwave.com>
2014-12-28 14:27:00 -0800

    Seeing if we can prevent the scriptlet failures

--- sprocket-api/rpm/deploy.sh
+++ sprocket-api/rpm/deploy.sh
@@ -11,9 +11,11 @@ fi
 while [ $EXTRA_TOMCAT -gt 0 ]
 do
  cp -p /etc/rc.d/init.d/tomcat7 /etc/rc.d/init.d/tomcat7-${EXTRA_TOMCAT}
+ chkconfig --del tomcat7-${EXTRA_TOMCAT} 2>&1 > /dev/null
  chkconfig --add tomcat7-${EXTRA_TOMCAT}
  chkconfig tomcat7-${EXTRA_TOMCAT} on
  service tomcat7-${EXTRA_TOMCAT} start
  EXTRA_TOMCAT=`expr ${EXTRA_TOMCAT} - 1`
 done
 
+exit 0
