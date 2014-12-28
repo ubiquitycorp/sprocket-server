c68865dc18f9a3fe34eb4f74218bfedf346b8f4c
Jonathan Kalbfeld <jonathan.kalbfeld@thoughtwave.com>
2014-12-28 11:33:02 -0800

    Trying to launch multiple tomcats using the built-in mechanism so we don't have to customize too much.

--- sprocket-api/rpm/deploy.sh
+++ sprocket-api/rpm/deploy.sh
@@ -1,2 +1,16 @@
 sleep 5
+EXTRA_TOMCAT=`expr ${tomcat.processes} - 1`
 service tomcat7 start
+
+if [ ! "$EXTRA_TOMCAT" ]
+then
+ EXTRA_TOMCAT=0
+fi
+
+while [ $EXTRA_TOMCAT -gt 0 ]
+do
+ chkconfig --add tomcat7-${EXTRA_TOMCAT}
+ service tomcat7-${EXTRA_TOMCAT} start
+ EXTRA_TOMCAT=`expr ${EXTRA_TOMCAT} - 1`
+done
+
--- sprocket-api/rpm/undeploy.sh
+++ sprocket-api/rpm/undeploy.sh
@@ -1,3 +1,6 @@
 sleep 5
 service tomcat7 stop
+service tomcat7-1 stop
+service tomcat7-2 stop
+service tomcat7-3 stop
 rm -rvf /var/lib/tomcat7/webapps/sprocket-api
