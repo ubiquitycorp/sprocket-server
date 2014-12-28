3ce6fb1d04a69981103cde16c371c45e199d1a90
Jonathan Kalbfeld <jonathan.kalbfeld@thoughtwave.com>
2014-12-28 13:38:05 -0800

    Testing logic to see if we can deploy multiple tomcats, on ports 8080-8083.

--- sprocket-api/rpm/undeploy.sh
+++ sprocket-api/rpm/undeploy.sh
@@ -3,4 +3,10 @@ service tomcat7 stop
 service tomcat7-1 stop
 service tomcat7-2 stop
 service tomcat7-3 stop
+chkconfig tomcat7-1 off
+chkconfig tomcat7-2 off
+chkconfig tomcat7-3 off
+chkconfig --del tomcat7-1
+chkconfig --del tomcat7-2
+chkconfig --del tomcat7-3
 rm -rvf /var/lib/tomcat7/webapps/sprocket-api
